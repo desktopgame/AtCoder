24793376dea73dbed213470ecbaa3d67531e373e
koya <gnndr36@gmail.com>
2022-06-18 21:50:52 +0900

    Update: iostream -> stdio

--- abc255/d.cpp
+++ abc255/d.cpp
@@ -1,32 +1,29 @@
 #include <cstdint>
-#include <iostream>
+#include <inttypes.h>
 #include <sstream>
+#include <stdio.h>
 #include <string>
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(0);
   int64_t N, Q;
-  std::cin >> N;
-  std::cin >> Q;
+  ::scanf("%" PRId64, &N);
+  ::scanf("%" PRId64, &Q);
   std::vector<int64_t> A;
   A.reserve(N);
   for (int i = 0; i < N; i++) {
     int64_t at;
-    std::cin >> at;
+    ::scanf("%" PRId64, &at);
     A.emplace_back(at);
   }
-  std::stringstream buf;
   for (int i = 0; i < Q; i++) {
     int64_t target;
-    std::cin >> target;
+    ::scanf("%" PRId64, &target);
     int64_t operations = 0;
     for (int64_t source : A) {
       operations += std::abs(target - source);
     }
-    buf << operations << "\n";
+    ::printf("%" PRId64 "\n", operations);
   }
-  std::cout << buf.str();
   return 0;
 }
