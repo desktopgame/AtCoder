4915c727d834f6202ecae67594a8ca93f9b6a5ca
koya <gnndr36@gmail.com>
2022-06-18 15:55:05 +0900

    Update: 二分探索で実装

--- abc255/c.py
+++ abc255/c.py
@@ -5,9 +5,20 @@
 # 公差とは、数列の数と数の間の差？
 # 項数とは数列の長さのこと
 import sys
+import math
 from typing import List
 
 params: List[int] = list(map(int, sys.stdin.readline().split(' ')))
 [X, A, D, N] = params
-U = [A + (D * i) for i in range(0, N)] # 初項に項数の長さ分公差を足す
-print(min(map(lambda v: abs(X - v), U)))
+
+def solve(start: int, end: int, vv1, vv2) -> int:
+    v1 = abs(A + (D * start) - X)
+    v2 = abs(A + (D * end) - X)
+    if v1 == vv1 and v2 == vv2:
+        return min([v1, v2])
+    if v1 < v2:
+        return solve(start, start + math.ceil((end - start) / 2), v1, v2)
+    else:
+        return solve(start + math.ceil((end - start) / 2), end, v1, v2)
+
+print(solve(0, N, N + 1, N + 1))
