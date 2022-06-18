7f99d7d5c8cfd29ac30e3cbba116cadc02aa918c
koya <gnndr36@gmail.com>
2022-06-18 18:24:27 +0900

    Update: 全部リストに入れないように

--- abc255/d.py
+++ abc255/d.py
@@ -4,8 +4,8 @@ from typing import List
 nq: List[int] = list(map(int, sys.stdin.readline().split(' ')))
 [N, Q] = nq
 A = list(map(int, sys.stdin.readline().split(' ')))
-X = list(map(int, [sys.stdin.readline() for i in range(0, Q)]))
-for target in X:
+for i in range(0, Q):
+    target = int(sys.stdin.readline())
     operations = 0
     for source in A:
         operations += abs(target - source)
