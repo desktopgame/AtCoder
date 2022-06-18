3c4daa93af288d05b38d2fa9fc5454ce2e3932b0
koya <gnndr36@gmail.com>
2022-06-18 21:23:44 +0900

    Add: C++の解法を追加

---
+++ abc255/d.cpp
@@ -0,0 +1,27 @@
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+  int64_t N, Q;
+  std::cin >> N;
+  std::cin >> Q;
+  std::vector<int64_t> A;
+  A.reserve(N);
+  for (int i = 0; i < N; i++) {
+    int64_t at;
+    std::cin >> at;
+    A.emplace_back(at);
+  }
+  for (int i = 0; i < Q; i++) {
+    int64_t target;
+    std::cin >> target;
+    int64_t operations = 0;
+    for (int64_t source : A) {
+      operations += std::abs(target - source);
+    }
+    std::cout << operations << std::endl;
+  }
+  return 0;
+}
