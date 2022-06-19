5ee48713a4db701136190330befff49078118f60
koya <gnndr36@gmail.com>
2022-06-19 14:27:39 +0900

    Update: abc255/d AC
    * bisectの実装が間違っていた？標準にしたらAC

--- abc255/d.py
+++ abc255/d.py
@@ -2,29 +2,12 @@
 # NOTE
 # https://atcoder.jp/contests/abc255/editorial/4109
 import sys
-import math
+import bisect
 from typing import List
 
 nq: List[int] = list(map(int, sys.stdin.readline().split(' ')))
 [N, Q] = nq
 A = list(map(int, sys.stdin.readline().split(' ')))
-
-# A[k] < X[i] < A[k+1]
-# となるような K を見つける
-# Xよりも大きい値がAの中に見つからなければ len(A) を返す
-def bisect(x: int, start: int, end: int) -> int:
-    if start >= len(A) or end >= len(A):
-        return len(A)
-    v1 = abs(A[start] - x)
-    v2 = abs(A[end] - x)
-    if (end - start) == 1:
-        if A[start] < x and A[end] < x:
-            return bisect(x, start + 1, end + 1)
-        return (start if x < A[start] else end)
-    if v1 < v2:
-        return bisect(x, start, start + math.ceil((end - start) / 2))
-    else:
-        return bisect(x, start + math.ceil((end - start) / 2), end)
 
 A.sort()
 # ループのたびにシグマを計算するとコストがかかるので累積和として持っておく
@@ -33,7 +16,7 @@ for index, i in enumerate(A):
     table.append(table[index] + A[index])
 for i in range(0, Q):
     x = int(sys.stdin.readline())
-    K = bisect(x, 0, N - 1)
+    K = bisect.bisect_left(A, x)
     # シグマ (X[i] - A[j]), (A[j] - X[i]) の計算
     w1 = K * x - table[K]
     w2 = -((N - K) * x) + table[N] - table[K]
